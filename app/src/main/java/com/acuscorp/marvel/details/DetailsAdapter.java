04a1502a94278f4b366d9f5c8090e5f375e7399a
noe adrian acuña prado <acuscorp@gmail.com>
2020-03-10 09:18:11 -0600

    Update DetailsAdapter.java

--- app/src/main/java/com/acuscorp/marvel/details/DetailsAdapter.java
+++ app/src/main/java/com/acuscorp/marvel/details/DetailsAdapter.java
@@ -1,22 +1,19 @@
+package com.acuscorp.marvel.details;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-        package com.acuscorp.marvel.details;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
 
-        import android.content.Context;
-        import android.view.LayoutInflater;
-        import android.view.View;
-        import android.view.ViewGroup;
-        import android.widget.ImageView;
-        import android.widget.TextView;
+import com.acuscorp.marvel.R;
+import com.bumptech.glide.RequestManager;
 
-        import androidx.annotation.NonNull;
-        import androidx.recyclerview.widget.RecyclerView;
-
-        import com.acuscorp.marvel.R;
-        import com.bumptech.glide.RequestManager;
-        import com.squareup.picasso.Picasso;
-
-        import java.util.List;
+import java.util.List;
 
 public class DetailsAdapter extends RecyclerView.Adapter<DetailsAdapter.DetailsHolder> {
 
@@ -25,7 +22,7 @@ public class DetailsAdapter extends RecyclerView.Adapter<DetailsAdapter.DetailsH
     private List<String> names;
     private RequestManager requestManager;
 
-    public DetailsAdapter(Context context, RequestManager requestManager,List<String> urls, List<String> names) {
+    public DetailsAdapter(Context context, RequestManager requestManager, List<String> urls, List<String> names) {
         this.context = context;
         this.urls = urls;
         this.names = names;
@@ -34,20 +31,14 @@ public class DetailsAdapter extends RecyclerView.Adapter<DetailsAdapter.DetailsH
 
     @NonNull
     @Override
-    public DetailsHolder onCreateViewHolder( ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(context).inflate(R.layout.cardview_comic_item,parent,false);
+    public DetailsHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.cardview_comic_item, parent, false);
         return new DetailsHolder(view);
     }
 
     @Override
-    public void onBindViewHolder( DetailsHolder holder, int position) {
-        String heroUrl = "https://firebasestorage.googleapis.com/v0/b/fir-ecb2b.appspot.com/o/uploads%2F1583631375309.jpg?alt=media&token=05680ca0-1447-4751-8c27-15d5e0a162ef";
-//        Picasso.get()
-//                .load(heroUrl)
-//                .placeholder(R.mipmap.ic_launcher_round)
-//                .fit()
-//                .centerCrop()
-//                .into(holder.mHeroComicImage);
+    public void onBindViewHolder(DetailsHolder holder, int position) {
+
         holder.mHeroName.setText(names.get(position));
 
         try {
@@ -66,10 +57,10 @@ public class DetailsAdapter extends RecyclerView.Adapter<DetailsAdapter.DetailsH
         return names.size();
     }
 
-    class DetailsHolder extends RecyclerView.ViewHolder{
-        private RequestManager requestManager;
+    class DetailsHolder extends RecyclerView.ViewHolder {
         private ImageView mHeroComicImage;
         private TextView mHeroName;
+
         public DetailsHolder(View itemView) {
             super(itemView);
             mHeroComicImage = itemView.findViewById(R.id.iv_comic_image);
@@ -78,8 +69,6 @@ public class DetailsAdapter extends RecyclerView.Adapter<DetailsAdapter.DetailsH
         }
 
     }
-
-
 
 
 }
